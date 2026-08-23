21177f6e0af8e9fa6f27d22c87ac9f8a8071b280
mosabber480 <mosabber480@gmail.com>
2026-08-23 17:27:34 +0600

    Revert output export in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
