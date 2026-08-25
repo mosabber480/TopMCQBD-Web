958c7e4f9c0d23b2c3eed7bceacfe44ebf366f7a
mosabber480 <mosabber480@gmail.com>
2026-08-25 19:40:34 +0600

    Clean up legacy cloudflare functions and test scratch files for pure Render Node.js backend

--- functions/api/categories.js
+++
@@ -1,6 +0,0 @@
-import { onRequest as routeHandler } from './[[route]].js';
-
-export async function onRequest(context) {
-  context.params = { ...(context.params || {}), route: ['categories'] };
-  return routeHandler(context);
-}
--- functions/api/questions/free.js
+++
@@ -1,6 +0,0 @@
-import { onRequest as routeHandler } from '../[[route]].js';
-
-export async function onRequest(context) {
-  context.params = { ...(context.params || {}), route: ['questions', 'free'] };
-  return routeHandler(context);
-}
