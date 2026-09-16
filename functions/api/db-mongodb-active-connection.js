c11a94a324652e63f773ef955531c40875aab6a2
mosabber480 <mosabber480@gmail.com>
2026-09-16 20:07:02 +0600

    fix(pages): remove heavy bson bundling from pages functions so cloudflare pages deploys cleanly

--- functions/api/db-mongodb-active-connection.js
+++ functions/api/db-mongodb-active-connection.js
@@ -4,8 +4,6 @@
  * Perfect for UptimeRobot (1-URL keep-alive ping) & Direct Automated Wakeup
  */
 
-import { pingEdgeCluster, CLUSTERS } from '../_shared/mongodb.js';
-
 const ENDPOINTS = [
   {
     id: 'paid',
